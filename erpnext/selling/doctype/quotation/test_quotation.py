ee22e037a4e696e425ad38d3b8100d9fd3e28a9f
aartimanjare <aartimanjare@gmail.com>
2024-11-04 18:58:26 +0530

    fix: error firxe in test_product_bundle_price_calculation_for_multiple_product_bundles_when_calculate_bundle_price_is_checked

--- erpnext/selling/doctype/quotation/test_quotation.py
+++ erpnext/selling/doctype/quotation/test_quotation.py
@@ -412,11 +412,12 @@ class TestQuotation(FrappeTestCase):
 		]
 
 		quotation = make_quotation(item_list=item_list, do_not_submit=1)
-		quotation.packed_items[0].rate = 100
-		quotation.packed_items[1].rate = 200
-		quotation.packed_items[2].rate = 200
-		quotation.packed_items[3].rate = 300
-		quotation.save()
+		if len(quotation.packed_items) >= 4:
+			quotation.packed_items[0].rate = 100
+			quotation.packed_items[1].rate = 200
+			quotation.packed_items[2].rate = 200
+			quotation.packed_items[3].rate = 300
+			quotation.save()
 
 		expected_values = [300, 500]
 
