2d1b63b7c0b9ffc01430a194dd5d555997432948
shubham-DT <shubham@8848digital.com>
2025-03-17 15:03:33 +0530

    chore: translatable strings

--- erpnext/regional/italy/utils.py
+++ erpnext/regional/italy/utils.py
@@ -263,7 +263,7 @@ def sales_invoice_validate(doc):
 	doc.company_fiscal_code = frappe.get_cached_value("Company", doc.company, "fiscal_code")
 	if not doc.company_tax_id or not doc.company_fiscal_code:
 		frappe.throw(
-			_(f"Please set both the Tax ID and Fiscal Code on Company {doc.company}"),
+			_("Please set both the Tax ID and Fiscal Code on Company {0}").format(doc.company),
 			title=_("E-Invoicing Information Missing"),
 		)
 	# Validate customer details
